a394a2af146a74c74d1497e3cd55caa5efb882ea
JW <kiw970923@naver.com>
2021-04-03 16:21:56 +0900

    앱 시작시 효과음 재생 initapp.mp3

--- app/src/main/java/com/example/crescendo_piano/SplashActivity.java
+++ app/src/main/java/com/example/crescendo_piano/SplashActivity.java
@@ -2,6 +2,8 @@ package com.example.crescendo_piano;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -12,21 +14,15 @@ import androidx.annotation.Nullable;
 public class SplashActivity extends Activity {
     private View decorView;
     private int uiOption;
+    int key_initsound;
+    SoundPool initsound;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
+        initsound=new SoundPool(1,AudioManager.STREAM_MUSIC,0);
         super.onCreate(savedInstanceState);
+        key_initsound=initsound.load(this, R.raw.initapp,1);
 
-        /////////////////////////////앱 하단바 제거///////////////////////////////
-        decorView=getWindow().getDecorView();
-        uiOption=getWindow().getDecorView().getSystemUiVisibility();
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-            uiOption|=View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN)
-            uiOption|=View.SYSTEM_UI_FLAG_FULLSCREEN;
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT)
-            uiOption|=View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOption);
-        /////////////////////////////앱 하단바 제거///////////////////////////////  // 앱 하단바 제거
+
 
         try{
             Thread.sleep(2400);
@@ -34,6 +30,9 @@ public class SplashActivity extends Activity {
             e.printStackTrace();
         }
         startActivity(new Intent(this, MainActivity.class));
+        initsound.play(key_initsound,1,1,0,0,1);
         finish();
     }
+
+
 }
