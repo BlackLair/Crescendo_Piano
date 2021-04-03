b241e0265cf2b647f4b22d504b3d8dfc14510f08
JW <kiw970923@naver.com>
2021-04-03 13:27:11 +0900

    앱 하단바 제거

--- app/src/main/java/com/example/crescendo_piano/SplashActivity.java
+++ app/src/main/java/com/example/crescendo_piano/SplashActivity.java
@@ -2,15 +2,31 @@ package com.example.crescendo_piano;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
 // 로딩화면 구현 클래스
 public class SplashActivity extends Activity {
+    private View decorView;
+    private int uiOption;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        /////////////////////////////앱 하단바 제거///////////////////////////////
+        decorView=getWindow().getDecorView();
+        uiOption=getWindow().getDecorView().getSystemUiVisibility();
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+            uiOption|=View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN)
+            uiOption|=View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT)
+            uiOption|=View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOption);
+        /////////////////////////////앱 하단바 제거///////////////////////////////  // 앱 하단바 제거
 
         try{
             Thread.sleep(2400);
