59d111623dab1743ed48937c103051edf1192b0a
JW <kiw970923@naver.com>
2021-04-23 16:26:11 +0900

    midi 연주모드에서 악기선택화면으로 되돌아오고 다시 악기 선택 시 app 화면상 연주로 바뀌는 버그 수정

--- app/src/main/java/com/example/crescendo_piano/MidiActivity.java
+++ app/src/main/java/com/example/crescendo_piano/MidiActivity.java
@@ -12,17 +12,22 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MidiActivity extends AppCompatActivity {
     private View decorView; // 전체화면 출력을 위한 멤버 변수
     private int uiOption;
+
+    ImageButton btn_goBack;
 
     private SoundResourceManager soundManager;
     public int[] soundKeys; // SoundPool 할당된 오디오 파일 구분 키값
     MidiManager midiManager;// MIDI 장치 연결 관리 매니저
     public SoundPool midiSoundPool; // 건반 사운드 리소스 로딩 SoundPool
     public static int keyboardChannel,drumChannel; // 설정할 midi channel
+    MyDeviceCallback myMidiCallback;
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +45,44 @@ public class MidiActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOption);
         /////////////////////////////앱 하단바 제거///////////////////////////////
         Intent getInst=getIntent();     // 선택한 악기 정보를 가져오기 위한 인텐트
-        keyboardChannel=1; drumChannel=10;
-        soundManager=new SoundResourceManager();
+        keyboardChannel=1; drumChannel=10;  // 채널 기본값 설정
+        soundManager=new SoundResourceManager();    // 사운드매니저 객체생성
         soundKeys=new int[88];
         midiSoundPool=soundManager.load(soundKeys, getInst.getIntExtra("inst", 0), getApplicationContext()); // 악기 음원 로딩
         midiManager=(MidiManager)getApplicationContext().getSystemService(Context.MIDI_SERVICE);
         MidiDeviceInfo[] deviceList=midiManager.getDevices();   // 연결되어 있는 장치 목록 가져옴
 
-        MyDeviceCallback myMidiCallback=new MyDeviceCallback(this, midiManager, getApplicationContext(),midiSoundPool ,soundKeys );
-        midiManager.registerDeviceCallback(myMidiCallback, Handler.createAsync(Looper.getMainLooper()));
+        myMidiCallback=new MyDeviceCallback(this, midiManager, getApplicationContext(),midiSoundPool ,soundKeys );
+        midiManager.registerDeviceCallback(myMidiCallback, Handler.createAsync(Looper.getMainLooper())); //디바이스 콜백 등록
 
+
+        btn_goBack=(ImageButton)findViewById(R.id.midi_goBack);
+        btn_goBack.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return false;
+            }
+        });
+        btn_goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MidiActivity.this, InstSelectActivity.class);
+                intent.putExtra("selectInst", 1); // midi연주모드
+                myMidiCallback.disConnect();    // Port Receiver 연결 해제
+                midiManager.unregisterDeviceCallback(myMidiCallback);   //콜백 해제
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onDestroy() {    // 액티비티 종료시
+
+        soundManager.unLoad(midiSoundPool); // 사운드 리소스 메모리 해제
+
+        super.onDestroy();
     }
 }
