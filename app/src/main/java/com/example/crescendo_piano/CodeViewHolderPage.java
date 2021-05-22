cf7969b2da12db4731a208dcd69b13be91e4b03f
JW <kiw970923@naver.com>
2021-05-22 17:26:28 +0900

    코드 선택 액티비티에서 그림자 눌러도 선택 안되게 변경

--- app/src/main/java/com/example/crescendo_piano/CodeViewHolderPage.java
+++ app/src/main/java/com/example/crescendo_piano/CodeViewHolderPage.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CodeViewHolderPage extends RecyclerView.ViewHolder {
     private ImageButton codeButton;
     private RelativeLayout code_layout;
+    private Float posY=0f;
     CodeDataPage data;
 
     CodeViewHolderPage(View itemView){
@@ -25,39 +26,46 @@ public class CodeViewHolderPage extends RecyclerView.ViewHolder {
         codeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {    // 코드 레시피 버튼 클릭 시
-                Intent intent=new Intent(data.getContext(), CodeRecipeActivity.class);
-                intent.putExtra("selectedCode", selectedCode);
-                data.getContext().startActivity(intent);
+                if(posY<600) {
+                    Intent intent = new Intent(data.getContext(), CodeRecipeActivity.class);
+                    intent.putExtra("selectedCode", selectedCode);
+                    data.getContext().startActivity(intent);
+                }
             }
         });
         codeButton.setOnTouchListener(new View.OnTouchListener() { // 코드 레시피 버튼 누를 때, 뗄 때 이미지 바뀌는 효과 넣기
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+
                 if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
-                    switch (selectedCode){
-                        case 0:
-                            codeButton.setBackgroundResource(R.drawable.code_cresc_one_p);
-                            break;
-                        case 1:
-                            codeButton.setBackgroundResource(R.drawable.code_cresc_two_p);
-                            break;
-                        case 2:
-                            codeButton.setBackgroundResource(R.drawable.code_cresc_three_p);
-                            break;
-                        case 3:
-                            codeButton.setBackgroundResource(R.drawable.code_london_p);
-                            break;
-                        case 4:
-                            codeButton.setBackgroundResource(R.drawable.code_paris_p);
-                            break;
-                        case 5:
-                            codeButton.setBackgroundResource(R.drawable.code_tokyo_p);
-                            break;
-                        case 6:
-                            codeButton.setBackgroundResource(R.drawable.code_la_p);
-                            break;
+                    posY=motionEvent.getY();
+                    if(posY<600) {
+                        switch (selectedCode) {
+                            case 0:
+                                codeButton.setBackgroundResource(R.drawable.code_cresc_one_p);
+                                break;
+                            case 1:
+                                codeButton.setBackgroundResource(R.drawable.code_cresc_two_p);
+                                break;
+                            case 2:
+                                codeButton.setBackgroundResource(R.drawable.code_cresc_three_p);
+                                break;
+                            case 3:
+                                codeButton.setBackgroundResource(R.drawable.code_london_p);
+                                break;
+                            case 4:
+                                codeButton.setBackgroundResource(R.drawable.code_paris_p);
+                                break;
+                            case 5:
+                                codeButton.setBackgroundResource(R.drawable.code_tokyo_p);
+                                break;
+                            case 6:
+                                codeButton.setBackgroundResource(R.drawable.code_la_p);
+                                break;
+                        }
                     }
                 }  else if(motionEvent.getAction()==MotionEvent.ACTION_UP || motionEvent.getAction()==MotionEvent.ACTION_CANCEL){
+                    posY=motionEvent.getY();
                     switch (selectedCode){                      // ACTION_UP은 손가락 뗐을 때, ACTION_CANCEL은 스크롤 했을 때
                         case 0:
                             codeButton.setBackgroundResource(R.drawable.code_cresc_one);
